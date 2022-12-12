f12eea6fedab7236ffd66020d0bd30eb4413db85
pugaljp <103755873+pugaljp@users.noreply.github.com>
2022-12-12 17:20:34 +0530

    Add files via upload

---
+++ day16task/script.js
@@ -0,0 +1,33 @@
+
+let countdown=10;  
+setTimeout(() => {
+    document. querySelector(".countdown").innerText = --countdown;
+    setTimeout(() => {
+        document. querySelector(".countdown").innerText = --countdown;
+        setTimeout(() => {
+            document. querySelector(".countdown").innerText = --countdown;
+            setTimeout(() => {
+                document. querySelector(".countdown").innerText = --countdown;
+                setTimeout(() => {
+                    document. querySelector(".countdown").innerText = --countdown;
+                    setTimeout(() => {
+                        document. querySelector(".countdown").innerText = --countdown;
+                        setTimeout(() => {
+                            document. querySelector(".countdown").innerText = --countdown;
+                            setTimeout(() => {
+                                document. querySelector(".countdown").innerText = --countdown;
+                                setTimeout(() => {
+                                    document. querySelector(".countdown").innerText = --countdown;
+                                    setTimeout(() => {
+                                        document. querySelector(".countdown").innerText = "happy independence day🎇🎉🎈";
+                                    },1000);                                
+                                 },1000);                            
+                             },1000);                        
+                         },1000);                    
+                     },1000);
+                },1000);
+            },1000);
+        },1000);
+     },1000);
+},1000);
+
